5bd0c7f175c54a03641e58f2aa3f7ca5333cd3ec
K.Ogawa <o46026473@gmail.com>
2017-02-17 18:23:20 +0900

    Initial Commit

---
+++ Assets/Scripts/NameButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void OnMouseEnter () {
+		Debug.Log("３秒間見続けてください。");
+		Invoke ( "SceneLoad2", 3.0f );
+	}
+
+	public void SceneLoad2 () {
+		Debug.Log("3秒経ちました。ログイン画面に移ります。");
+		SceneManager.LoadScene ("LoginView");
+	}
+
+	public void OnMouseExit () {
+		Debug.Log("選択をキャンセルしました。");
+		CancelInvoke ();
+	}
+}
